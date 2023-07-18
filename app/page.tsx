d948dda4f758bba8e29e0eba5dc42a8eb7af4c7c
Canaris <mrgeek484@gmail.com>
2023-07-18 21:27:51 +0800

    gg

---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
+
+export default function Home() {
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	// display webcam in canvas
+	useEffect(() => {
+		(async () => {
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: {
+					frameRate: {
+						exact: 10,
+					},
+				},
+			});
+
+			// display the video in the canvas
+			const canvas = canvasRef.current;
+			if (!canvas) return;
+			const ctx = canvas.getContext('2d');
+			if (!ctx) return;
+			const video = document.createElement('video');
+			video.srcObject = stream;
+			video.play();
+			setInterval(() => {
+				ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+				const imageData = ctx.getImageData(
+					0,
+					0,
+					canvas.width,
+					canvas.height,
+				);
+
+				// unint8 array
+				// this one you faggot
+				// data is unint8clampedarray
+				const data = imageData.data;
+				console.log(data);
+			}, 1000);
+		})();
+	}, []);
+
+	return (
+		<div className='w-full h-96'>
+			<canvas className='h-full' id='canvas' ref={canvasRef}></canvas>
+		</div>
+	);
+}
