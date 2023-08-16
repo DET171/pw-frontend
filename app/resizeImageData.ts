ccb93a87ad325b1e6966f666f78ddb9c459db153
Canaris <mrgeek484@gmail.com>
2023-08-16 21:29:41 +0800

    jesus CHRIST

--- app/resizeImageData.ts
+++ app/resizeImageData.ts
@@ -54,14 +54,32 @@
 // }
 
 // write a function called resizeImageDataToMultiplesOf16 that takes an ImageData object and returns a new ImageData object that is the same as the original but with the width and height rounded down to the nearest multiple of 16.
-export default function resizeImageDataToMultiplesOf16(imageData: ImageData) {
-	const width = imageData.width & ~15;
-	const height = imageData.height & ~15;
+// export default function resizeImageDataToMultiplesOf16(imageData: ImageData) {
+// 	const width = imageData.width & ~15;
+// 	const height = imageData.height & ~15;
+// 	const canvas = document.createElement('canvas');
+// 	canvas.width = width;
+// 	canvas.height = height;
+// 	const ctx = canvas.getContext('2d');
+//   ctx!.putImageData(imageData, 0, 0);
+//   const resizedImageData = ctx!.getImageData(0, 0, width, height);
+//   return resizedImageData;
+// }
+
+export default function resizeImageDataTo224x224(imageData: ImageData) {
 	const canvas = document.createElement('canvas');
-	canvas.width = width;
-	canvas.height = height;
+	canvas.width = 224;
+	canvas.height = 224;
 	const ctx = canvas.getContext('2d');
-  ctx!.putImageData(imageData, 0, 0);
-  const resizedImageData = ctx!.getImageData(0, 0, width, height);
-  return resizedImageData;
+  ctx!.imageSmoothingQuality = 'high';
+  ctx!.imageSmoothingEnabled = true;
+  const tempCanvas = document.createElement('canvas');
+  tempCanvas.width = imageData.width;
+  tempCanvas.height = imageData.height;
+  const tempCtx = tempCanvas.getContext('2d');
+  tempCtx!.putImageData(imageData, 0, 0);
+  const x = Math.floor((canvas.width - imageData.width) / 2);
+  const y = Math.floor((canvas.height - imageData.height) / 2);
+  ctx!.drawImage(tempCanvas, x, y);
+  return ctx!.getImageData(0, 0, canvas.width, canvas.height);
 }
